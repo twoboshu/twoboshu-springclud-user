bd8977d647d8c3525fd3f4572046f7e62c35db20
lilin <lilin123>
2017-04-13 16:36:27 +0800

    初始化

---
+++ src/main/java/com/twoboshu/Application.java
@@ -0,0 +1,12 @@
+package com.twoboshu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+	    System.out.print("12121");
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/test/java/com/twoboshu/TwoboshuSpringcludUserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.twoboshu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwoboshuSpringcludUserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
